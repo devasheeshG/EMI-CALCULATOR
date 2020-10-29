4210f13ad1ac8cad067a2a4e070e95a423603930
devasheeshG <devasheeshG@github.com>
2020-10-29 15:25:29 +0530

    added all code

---
+++ files/EMI CALCULATOR/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim a, b, c As Double
+        Dim d As Integer
+
+        If IsNumeric(TextBox1.Text) Then
+            a = Convert.ToDouble(TextBox1.Text)
+        Else
+            MsgBox("Invalid amount, please re-enter")
+            TextBox1.Focus()
+            TextBox1.SelectAll()
+
+        End If
+        If IsNumeric(TextBox2.Text) Then
+            d = Convert.ToInt32(TextBox2.Text)
+        Else
+            MsgBox("Please re-enter the number in months")
+            TextBox1.Focus()
+            TextBox1.SelectAll()
+
+        End If
+        If IsNumeric(TextBox3.Text) Then
+            b = 0.01 * Convert.ToDouble(TextBox3.Text) / 12
+        Else
+            MsgBox("Invalid interest rate, please re-enter")
+            TextBox1.Focus()
+            TextBox1.SelectAll()
+
+        End If
+        c = Pmt(b, d, -a, 0)
+        TextBox4.Text = c.ToString("#.00")
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+End Class
---
+++ files/EMI CALCULATOR/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EMI CALCULATOR")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("EMI CALCULATOR")> 
+<Assembly: AssemblyCopyright("Copyright ©  2020")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c2e1026a-d672-434c-bad2-43d92eabc94f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
